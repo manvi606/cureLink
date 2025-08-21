96c736abf3f6d820bb5cd1225b6b4ca639addf7c
Manvi Singh <9305259173.manvi@gmail.com>
2025-08-21 20:21:15 +0530

    home page is compleated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,10 +8,13 @@ import Contact from './pages/Contact'
 import MyProfile from './pages/MyProfile'
 import MyAppointments from './pages/MyAppointments'
 import Appointment from './pages/Appointment'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctors />} />
@@ -24,6 +27,7 @@ const App = () => {
         <Route path="/appoinments/:docId" element={<Appointment/>} />
         
       </Routes>
+      <Footer/>
     </div>
   )
 }
